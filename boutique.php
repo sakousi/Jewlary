62521441ebed7608d08e77de857aad39a55cb525
theau <theaudugueperoux@gmail.com>
2023-07-10 17:30:44 +0200

    style class store

--- boutique.php
+++ boutique.php
@@ -7,14 +7,14 @@
     </div>
 </div>
 
-<section>
-    <h2>Nouvelle collections</h2>
-    <div>
-        <article>
-            <img src="assets/img/collier1.jpg" alt="collier1">
-            <h3>Collier 1</h3>
-            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Corporis corrupti consequuntur non, quas cumque, porro numquam cum, delectus doloribus dolorum quae distinctio facere fugiat illo obcaecati tenetur error atque reiciendis!</p>
-            <button type="button">Acheter</button>
+<section class="container newcollection">
+    <h2 class="newcollection__title">Nouvelle collections</h2>
+    <div class="newcollection__cards">
+        <article class="newcollection__cards_card">
+            <img class="newcollection__cards_card_image" src="assets/img/collier1.jpg" alt="collier1">
+            <h3 class="newcollection__cards_card_title">Collier 1</h3>
+            <p class="newcollection__cards_card_text">Lorem ipsum dolor sit amet consectetur adipisicing elit. Corporis corrupti consequuntur non, quas cumque, porro numquam cum, delectus doloribus dolorum quae distinctio facere fugiat illo obcaecati tenetur error atque reiciendis!</p>
+            <button class="newcollection__cards_card_btn" type="button">Acheter</button>
         </article>
     </div>
 </section>
