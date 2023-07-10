03c12abeee32bd987145373b2f8c12f7e5f9d538
theau <theaudugueperoux@gmail.com>
2023-07-10 17:23:28 +0200

    footer

--- footer.php
+++ footer.php
@@ -1,5 +1,13 @@
-    <footer>
-        
+    <footer class="footer">
+        <ul>
+            <li><a href="index.php">Home</a></li>
+            <li><a href="index.php">Boutique</a></li>
+            <li><a href="index.php">Qui suis-je</a></li>
+        </ul>
+        <ul>
+            <!-- legal -->
+            <li><a href="">Mentions legal</a></li>
+        </ul>
     </footer>
 </body>
 </html>
